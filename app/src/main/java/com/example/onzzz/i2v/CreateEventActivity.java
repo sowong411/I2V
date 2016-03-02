12c4cd6e84a5971b0a542db5a7b47af662ffebee
Wong Sui On <sowong411@gmail.com>
2016-03-02 21:05:29 +0800

    Store Event Information to Parse

--- app/src/main/java/com/example/onzzz/i2v/CreateEventActivity.java
+++ app/src/main/java/com/example/onzzz/i2v/CreateEventActivity.java
@@ -1,5 +1,6 @@
 package com.example.onzzz.i2v;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -7,38 +8,105 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by WAICHONG on 31/12/2015.
  */
 public class CreateEventActivity extends ActionBarActivity {
 
+    String userObjectId;
+    String eventObjectId;
+    String eventName;
+
+    int newEventIndex;
+    String[] memberId = new String[20];
+    int numOfMember = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
         Intent intent = getIntent();
+        userObjectId = intent.getStringExtra("UserObjectId");
+
+        memberId[0] = userObjectId;
+
         assert (intent != null);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
         // after typing all info (date time place , app will create event)
         findViewById(R.id.create_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(CreateEventActivity.this, EventActivity.class);
-                startActivity(intent);
+                EditText eventNameText = (EditText) findViewById(R.id.name);
+                eventName = eventNameText.getText().toString();
+                uploadPhoto();
+                updateUserEvent();
+                eventNameText.setText("");
             }
         });
 
         findViewById(R.id.addmem_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(CreateEventActivity.this,AddMemberActivity.class);
-                startActivity(intent);
+                Intent i = new Intent(CreateEventActivity.this, AddMemberActivity.class);
+                startActivityForResult(i, 100);
+            }
+        });
+    }
+
+    public void uploadPhoto(){
+        ParseObject event = new ParseObject("Event");
+        event.put("EventName", eventName);
+        event.put("Date", "");
+        event.put("Time", "");
+        event.put("PhotoNumber", 0);
+        event.put("MemberNumber", numOfMember);
+        event.addAllUnique("Member", Arrays.asList(memberId));
+        event.put("EventHolder", memberId[0]);
+        event.put("VideoNumber", 0);
+        event.put("Video", "");
+        event.saveInBackground();
+    }
+
+    public void updateUserEvent(){
+        ParseQuery<ParseObject> eventQuery = ParseQuery.getQuery("Event");
+        eventQuery.whereExists("EventName");
+        eventQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                newEventIndex = objects.size()-1;
+                eventObjectId = objects.get(newEventIndex).getObjectId();
+                for (int i=0; i<numOfMember; i++){
+                    ParseQuery<ParseObject> accountQuery = ParseQuery.getQuery("Account");
+                    accountQuery.getInBackground(memberId[i], new GetCallback<ParseObject>() {
+                        @Override
+                        public void done(ParseObject object, ParseException e) {
+                            if (e == null) {
+                                object.add("Event", eventObjectId);
+                                object.saveInBackground();
+                                Intent intent = new Intent();
+                                intent.setClass(CreateEventActivity.this, EventActivity.class);
+                                intent.putExtra("UserObjectId", userObjectId);
+                                intent.putExtra("EventObjectId", eventObjectId);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
             }
         });
     }
@@ -72,11 +140,25 @@ public class CreateEventActivity extends ActionBarActivity {
                 return super.onOptionsItemSelected(item);
         }
         //  return super.onOptionsItemSelected(item);
-
-
-
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case (100) : {
+                if (resultCode == Activity.RESULT_OK) {
+                    String[] newText = data.getStringArrayExtra("Member");
+                    numOfMember = data.getIntExtra("NumOfMember", 1);
+                    // TODO Update your TextView.
+                    for (int i=1; i<numOfMember; i++){
+                        memberId[i] = newText[i];
+                    }
+                }
+                break;
+            }
+        }
+    }
 
     public void Close(View view) {
         finish();
